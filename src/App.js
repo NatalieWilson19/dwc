2ae4951c3342a39c051bf8b81ed18dad4bb48f43
Natalie Wilson <nataliewilson519@gmail.com>
2021-07-08 15:17:20 -0700

    Added basic Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home.js'
+import NavMenu from './components/NavMenu.js'
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <NavMenu />
     </div>
   );
 }
---
+++ src/components/NavMenu.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import "./NavMenu.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+/* I used this website: https://getwaves.io/ to generate the svg code to create the wave */
+
+/* Explanation for why rel="noopener" is important: https://mathiasbynens.github.io/rel-noopener/#hax */
+
+/*TODO change font of name */
+
+function NavMenu() {
+
+  return (
+    <div>
+        <Navbar className="navigation" expand="lg" fixed="top">
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                
+            <Nav.Link href="/" >Home</Nav.Link>
+            <Nav.Link href="#services" id="projects-id">Services</Nav.Link>
+            <Nav.Link href="#contact" id="contact-id">Contact</Nav.Link>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+}
+export default NavMenu
