88546085eaffd135ec4733443af9cf4e7255b7ab
Natalie Wilson <nataliewilson519@gmail.com>
2021-07-15 11:26:27 -0700

    Updated contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,8 @@
 import React from "react";
 import './Contact.css'
+import Location from '../images/icons8-marker-96.png'
+import Phone from '../images/icons8-phone-100.png'
+import Mail from '../images/icons8-mail-100.png'
 
 function Contact() {
     return(
@@ -8,10 +11,17 @@ function Contact() {
             <p>
                 Contact Us
             </p>
-        <div className="text">
-            <p> Contact us for any of your waterpoofing needs </p>
-            <a href="mailto:dwcbuild@gmail.com" target="_blank" rel="noopener noreferrer" />
-        </div>
+ 
+
+        <div className="subtext">
+            <p> Contact us for any of your waterpoofing needs. We are based in San Rafael, CA and are available for work in Marin and Sonoma County, San Francisco, and the East Bay. </p>
+            </div>
+            </div>
+        <div className="whereContact">
+        
+            <p> <img src={Location} className="location-icon" alt="location"></img>Based in San Rafael, CA</p>
+            <p> <img src={Phone} className="phone-icon" alt="phone"></img>Call at (415) 302 5542</p>
+            <p> <img src={Mail} className="mail-icon" alt="mail"></img>Email dwcbuild@gmail.com or click <a href="mailto:dwcbuild@gmail.com" target="_blank" rel="noopener noreferrer"> here </a> </p>
         </div>
         </div>
     );
