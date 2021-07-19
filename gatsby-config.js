d447c3605d4445a12e38af6cf2418d2c59d27073
Natalie Wilson <nataliewilson519@gmail.com>
2021-07-19 14:23:15 -0700

    Update sitemap config

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,48 +13,39 @@ module.exports = {
       options: {
         query: `
         {
-          allSitePage {
-            nodes {
-              path
+          site {
+            siteMetaData {
+              siteUrl
             }
           }
-          allWpContentNode(filter: {nodeType: {in: ["Post", "Page"]}}) {
-            nodes {
-              ... on WpPost {
-                uri
-                modifiedGmt
-              }
-              ... on WpPage {
-                uri
-                modifiedGmt
+          allSitePage {
+            edges {
+              node {
+                path
               }
             }
           }
         }
       `,
-        resolveSiteUrl: () => siteUrl,
-        resolvePages: ({
-          allSitePage: { nodes: allPages },
-          allWpContentNode: { nodes: allWpNodes },
-        }) => {
-          const wpNodeMap = allWpNodes.reduce((acc, node) => {
-            const { uri } = node
-            acc[uri] = node
-
-            return acc
-          }, {})
-
-          return allPages.map(page => {
-            return { ...page, ...wpNodeMap[page.path] }
-          })
-        },
-        serialize: ({ path, modifiedGmt }) => {
-          return {
-            url: path,
-            lastmod: modifiedGmt,
+      serialize: ({ site, allSitePage }) => {
+        const links = [];
+        for (let i = 0; i < allSitePage.edges.length; i++) {
+          const { path } = allSitePage.edges[i].node;
+          if (
+            /products|404|unsupported|account/.test(path)
+          ) {
+            continue;
+          } else if (/en-us|es-us/.test(path)) {
+            links.push({
+              url: site.siteMetadata.siteUrl + path,
+              changefreq: 'daily',
+              priority: 0.8,
+            });
           }
-        },
+        }
+        return links;
       },
     },
+  },
   ],
 }
