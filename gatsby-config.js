c1619ad92e11844a98ca277affe1b1dcca48c1e9
Natalie Wilson <nataliewilson519@gmail.com>
2021-07-19 14:42:42 -0700

    Added resolvePages

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,21 @@ module.exports = {
         }
       `,
       resolveSiteUrl: () => siteUrl,
+      resolvePages: ({
+        allSitePage: { nodes: allPages },
+        allWpContentNode: { nodes: allWpNodes },
+      }) => {
+        const wpNodeMap = allWpNodes.reduce((acc, node) => {
+          const { uri } = node
+          acc[uri] = node
+
+          return acc
+        }, {})
+
+        return allPages.map(page => {
+          return { ...page, ...wpNodeMap[page.path] }
+        })
+      },
       serialize: ({ site, allSitePage }) => {
         const links = [];
         for (let i = 0; i < allSitePage.edges.length; i++) {
