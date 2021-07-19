6d954b71b815a4505a77ae152faea7adb9285648
Natalie Wilson <nataliewilson519@gmail.com>
2021-07-19 13:51:42 -0700

    sitemap hopefully will be able to see all pages now

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,54 @@ module.exports = {
     author: 'Natalie Wilson, BS',
     siteUrl: 'https://dwcbuild.biz'
   },
-  plugins: ["gatsby-plugin-gatsby-cloud", "gatsby-plugin-sitemap"],
-};
+  plugins: ["gatsby-plugin-gatsby-cloud",
+  {
+    resolve: "gatsby-plugin-sitemap",
+      options: {
+        query: `
+        {
+          allSitePage {
+            nodes {
+              path
+            }
+          }
+          allWpContentNode(filter: {nodeType: {in: ["Post", "Page"]}}) {
+            nodes {
+              ... on WpPost {
+                uri
+                modifiedGmt
+              }
+              ... on WpPage {
+                uri
+                modifiedGmt
+              }
+            }
+          }
+        }
+      `,
+        resolveSiteUrl: () => siteUrl,
+        resolvePages: ({
+          allSitePage: { nodes: allPages },
+          allWpContentNode: { nodes: allWpNodes },
+        }) => {
+          const wpNodeMap = allWpNodes.reduce((acc, node) => {
+            const { uri } = node
+            acc[uri] = node
+
+            return acc
+          }, {})
+
+          return allPages.map(page => {
+            return { ...page, ...wpNodeMap[page.path] }
+          })
+        },
+        serialize: ({ path, modifiedGmt }) => {
+          return {
+            url: path,
+            lastmod: modifiedGmt,
+          }
+        },
+      },
+    },
+  ],
+}
