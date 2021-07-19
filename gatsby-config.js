72a6a97190e9aee4b1e359b1963fc80f2ea637e6
Natalie Wilson <nataliewilson519@gmail.com>
2021-07-19 13:35:10 -0700

    Added sitemap with gatsby sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  siteMetadata: {
+    title: "Duncan Wilson Construction",
+    descrption: 'General construction company that specalizes in waterproofing.',
+    author: 'Natalie Wilson, BS',
+    siteUrl: 'https://dwcbuild.biz'
+  },
+  plugins: ["gatsby-plugin-gatsby-cloud", "gatsby-plugin-sitemap"],
 };
