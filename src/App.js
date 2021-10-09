9f53a9c8bf9781958f7b2333a77d890953210732
Natalie Wilson <nataliewilson519@gmail.com>
2021-10-09 11:31:20 -0700

    Got rid of duplicate instances of components

--- src/App.js
+++ src/App.js
@@ -8,19 +8,17 @@ import About from './components/About.js'
 
 
 /* Apparently since Gatsby handles routing for you, it dislikes you trying to use 'react-router-dom' so I'll remove
-and see it it fixes */
+and see it it fixes
+https://github.com/gatsbyjs/gatsby/issues/12706 */
+
 function App() {
   return (
     <div className="App-Router">
       <NavMenu />
       <Home />
             <Services />
-            <Services />
-            <About />
             <About />
             <Contact />
-          <Contact />
-
     </div>
   );
 }
